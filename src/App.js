b3202c25f36037cc28c38cf9cad84364341d68b5
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-04-02 21:05:02 +0100

    added navbar to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
+import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <main>
       <Sidebar />
+      <Dashboard />
     </main>
   );
 };
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./styles/dashboard.scss";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard__section">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Dashboard;
